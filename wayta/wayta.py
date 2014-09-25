8def49d159055b102a2878b389a35fc6712bc845
fabiobatalha <fabiobatalha@gmail.com>
2014-09-25 16:49:57 -0300

    Ajustes para subir aplicação com Gunicorn

--- wayta/wayta.py
+++ wayta/wayta.py
@@ -1,14 +1,10 @@
 import os
-from wsgiref.simple_server import make_server
 
 import pyramid.httpexceptions as exc
-from pyramid.config import Configurator
 from pyramid.view import view_config, notfound_view_config
 from pyramid.response import Response
 from pyramid.httpexceptions import HTTPNotFound
-from elasticsearch import Elasticsearch
 
-import utils
 import controller
 
 
@@ -74,38 +70,3 @@ def country(request):
         result = request.databroker.similar_countries('countries', query)
 
     return result
-
-
-def main(settings):
-    """ This function returns a Pyramid WSGI application.
-    """
-
-    
-    def add_databroker(request):
-
-        hosts = [
-            {
-                'host': settings['app:main']['elasticsearch_host'],
-                'port': settings['app:main']['elasticsearch_port']
-            }
-        ]
-
-        es = Elasticsearch(hosts)
-
-        return controller.DataBroker(es)
-
-    config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('institution', '/api/v1/institution')
-    config.add_route('country', '/api/v1/country')
-    config.add_request_method(add_databroker, 'databroker', reify=True)
-
-    config.scan()
-    return config.make_wsgi_app()
-
-config = utils.Configuration.from_file(os.environ.get('CONFIG_INI', os.path.dirname(__file__)+'/../config.ini'))
-
-settings = dict(config.items())
-app = main(settings)
